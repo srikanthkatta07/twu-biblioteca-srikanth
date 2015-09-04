832e638dbdb683e41c9853acff53b2eff21a95c2
srikantk <kaanth.katta@gmail.com>
2015-09-04 14:56:48 +0530

    should display when more than one book is in booklist

--- src/com/twu/biblioteca/DisplayBookList.java
+++ src/com/twu/biblioteca/DisplayBookList.java
@@ -1,13 +1,18 @@
+//To display the list of books
 package com.twu.biblioteca;
 
 import java.util.ArrayList;
 
 public class DisplayBookList {
+    private ArrayList<String> listOfBooks;
+
     public DisplayBookList(ArrayList<String> listOfBooks) {
+        this.listOfBooks = listOfBooks;
 
     }
 
     public void displayBookList() {
-        System.out.println("First book");
+        for (String book : listOfBooks)
+            System.out.println(book);
     }
 }
--- test/com/twu/biblioteca/DisplayBookListTest.java
+++ test/com/twu/biblioteca/DisplayBookListTest.java
@@ -31,7 +31,22 @@ public class DisplayBookListTest {
 
         display.displayBookList();
 
-        assertEquals("First book\n",outContent.toString() );
+        assertEquals("First book\n", outContent.toString());
+    }
+
+    @Test
+    public void shouldDisplayListOfBooksWhenMoreThanOneBook() {
+        ArrayList<String> listOfBooks = new ArrayList<String>();
+
+        listOfBooks.add("First book");
+        listOfBooks.add("Second book");
+        listOfBooks.add("Third book");
+
+        DisplayBookList display = new DisplayBookList(listOfBooks);
+
+        display.displayBookList();
+
+        assertEquals("First book\nSecond book\nThird book\n", outContent.toString());
     }
 
 
