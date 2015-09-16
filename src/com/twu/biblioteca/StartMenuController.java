0129c8d036aa89bcf785849a039f793e58c5d32a
srikantk <kaanth.katta@gmail.com>
2015-09-16 12:55:00 +0530

    adding one more delegation to deleagate the default user option

--- src/com/twu/biblioteca/StartMenuController.java
+++ src/com/twu/biblioteca/StartMenuController.java
@@ -2,15 +2,24 @@ package com.twu.biblioteca;
 
 public class StartMenuController {
 
+    private final MainMenu mainMenu;
+    private final Controller controller;
     private LoginAuthenticator loginAuthenticator;
     private ConsoleInput consoleInput;
 
-    public StartMenuController(ConsoleInput consoleInput, LoginAuthenticator loginAuthenticator) {
+    public StartMenuController(ConsoleInput consoleInput, LoginAuthenticator loginAuthenticator,MainMenu mainMenu,Controller controller) {
         this.consoleInput = consoleInput;
         this.loginAuthenticator = loginAuthenticator;
+        this.mainMenu=mainMenu;
+        this.controller=controller;
     }
 
     public void delegate(String option) {
-        loginAuthenticator.authenticate(consoleInput.takeInput(), consoleInput.takeInput());
+        if(option.equals("1"))
+            loginAuthenticator.authenticate(consoleInput.takeInput(), consoleInput.takeInput());
+        else {
+            mainMenu.showMenuList();
+            controller.delegate(consoleInput.takeInput());
+        }
     }
 }
--- test/com/twu/biblioteca/StartMenuControllerTest.java
+++ test/com/twu/biblioteca/StartMenuControllerTest.java
@@ -12,11 +12,26 @@ public class StartMenuControllerTest {
     public void shouldDelegateToTheUserLoginWhenUserEntersOptionLogin() {
         ConsoleInput consoleInput = mock(ConsoleInput.class);
         LoginAuthenticator loginAuthenticator = mock(LoginAuthenticator.class);
-        StartMenuController startMenuController = new StartMenuController(consoleInput, loginAuthenticator);
+        MainMenu mainMenu = mock(MainMenu.class);
+        Controller controller = mock(Controller.class);
+        StartMenuController startMenuController = new StartMenuController(consoleInput, loginAuthenticator, mainMenu,controller);
 
         startMenuController.delegate("1");
 
-        verify(loginAuthenticator, times(1)).authenticate(null,null);
+        verify(loginAuthenticator, times(1)).authenticate(null, null);
+    }
+
+    @Test
+    public void shouldDelegateToMainControllerOptionWhenUserEntersAsGuestOption() {
+        ConsoleInput consoleInput = mock(ConsoleInput.class);
+        LoginAuthenticator loginAuthenticator = mock(LoginAuthenticator.class);
+        MainMenu mainMenu = mock(MainMenu.class);
+        Controller controller = mock(Controller.class);
+        StartMenuController startMenuController = new StartMenuController(consoleInput, loginAuthenticator, mainMenu,controller);
+
+        startMenuController.delegate("2");
+
+        verify(mainMenu, times(1)).showMenuList();
     }
 
 }
